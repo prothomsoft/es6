896ea627502611c895dcdc402961aadbf92ecae3
Tomasz Prokop <tomasz.prokop@lumesse.com>
2016-12-06 11:28:18 +0100

    babel

---
+++ build/main.bundle.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    if (rate) {
+        var monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+    return monthlyPayment;
+};
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var change = "change1";
+    var test = "test";
+    var test2 = "test2";
+
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+});
